aa3245b9ead428d484b5e53b908db5dc8945c95d
nhoctienone1 <92415626+nhoctienone1@users.noreply.github.com>
2022-03-07 20:53:17 +0700

    upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Bé gái';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100009157854005';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng Bé Lan Anh';
+const contentCard = 'Chúc mừng ngày Quốc tế phụ nữ 8/3. Chúc em gái mạnh khoẻ, hạnh phúc, ngày càng xinh đẹp, gặt hái được nhiều thành công trong công việc, học tập.';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
